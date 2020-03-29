00e28278291ec4e500aa8d87c215f2e1c616167b
Song Wook <songtri@gmail.com>
2020-03-29 16:58:11 -0400

    basic movement and animation
    Added some movement and animation feature

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	private Animator animator;
+
+	private bool IsMovable
+	{
+		get
+		{
+			var animState = animator.GetCurrentAnimatorStateInfo(0);
+			return animState.IsName("Idle") || animState.IsName("Walk");
+		}
+	}
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	public void Move(Vector2 direction, bool move)
+	{
+		if (move && IsMovable)
+		{
+			Vector3 forward = transform.forward;
+			Vector3 movement = Vector3.zero;
+			if(direction.x != 0)
+				movement = new Vector3(forward.x * direction.x, 0f, forward.z * direction.x) * Time.deltaTime;
+			else if(direction.y != 0)
+				movement = new Vector3(forward.z * direction.y, 0f, - forward.x * direction.y) * Time.deltaTime;
+			transform.localPosition += movement;
+		}
+		animator.SetBool("SetWalking", move);
+	}
+
+	public void Rotate(float xAngle)
+	{
+		transform.Rotate(0f, xAngle, 0f);
+	}
+
+	public void Attack()
+	{
+		animator.SetBool("SetAttack", true);
+	}
+}
---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject CharacterPrefab = null;
+
+	[SerializeField]
+	private Camera mainCamera = null;
+
+	private Character mainPlayer = null;
+
+	private void Start()
+	{
+		GameObject go = Instantiate(CharacterPrefab);
+		mainPlayer = go.GetComponent<Character>();
+		mainCamera.transform.parent = mainPlayer.transform;
+		mainCamera.transform.localScale = Vector3.one;
+		mainCamera.transform.localPosition = new Vector3(0, 0.1f, -0.2f);
+		mainCamera.transform.forward = mainPlayer.transform.forward;
+	}
+
+	private void Update()
+	{
+		UpdateCamera();
+		CheckInput();
+	}
+
+	private void UpdateCamera()
+	{
+		//mainCamera.transform.position = mainPlayer.transform.position;
+		//mainCamera.transform.SetPositionAndRotation(mainPlayer.transform.position + new Vector3(0f, 0.1f, -0.15f), mainPlayer.transform.rotation);
+	}
+
+	private void CheckInput()
+	{
+		int forwardMove = 0;
+		int sideMove = 0;
+		if (Input.GetKey(KeyCode.W))
+			forwardMove = 1;
+		else if (Input.GetKeyUp(KeyCode.W))
+			forwardMove = 0;
+		if (Input.GetKey(KeyCode.A))
+			sideMove = -1;
+		else if (Input.GetKeyUp(KeyCode.A))
+			sideMove = 0;
+		if (Input.GetKey(KeyCode.S))
+			forwardMove = -1;
+		else if (Input.GetKeyUp(KeyCode.S))
+			forwardMove = 0;
+		if (Input.GetKey(KeyCode.D))
+			sideMove = 1;
+		else if (Input.GetKeyUp(KeyCode.D))
+			sideMove = 0;
+
+		Vector2 direction = new Vector2(forwardMove, sideMove);
+		bool move = forwardMove != 0 || sideMove != 0;
+		mainPlayer.Move(direction, move);
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			mainPlayer.Attack();
+		}
+
+		float xAngle = Input.GetAxis("Mouse X");
+		mainPlayer.Rotate(xAngle);
+	}
+}
